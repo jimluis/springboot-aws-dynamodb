8a6e031cdc05ff3fbe5caf39b268610d64d0771c
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-11-04 11:23:02 -0500

    Initial commit

---
+++ src/test/java/com/jimluisf/springbootawsdynamodb/SpringbootAwsDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimluisf.springbootawsdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
